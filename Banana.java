d471edefa16f91f8685aeb6f3ecabd0f085bdfec
341198687 <167599652+341198687@users.noreply.github.com>
2024-04-25 21:19:55 -0400

    Add sad face

--- Banana.java
+++ Banana.java
@@ -10,6 +10,13 @@ public class Banana extends Actor
         if(getX() <= 0) {
             resetBanana();
         }
+        
+        if(isTouching(Hero.class)){
+            // Add a game over the symbol 
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);
+        }
     }
     
     public void resetBanana() {
